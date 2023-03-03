39dc6fd578cdaffe6236dcc87ab0995c7b1dd113
Arturo Murguia Espinoza <bofo_cona@hotmail.com>
2023-03-02 18:27:37 -0600

    SE desarrolla ventana principal

---
+++ Precios_Turnos/ConfigDiseno.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Precios_Turnos
+{
+    /// <summary>
+    /// Lógica de interacción para ConfigDiseno.xaml
+    /// </summary>
+    public partial class ConfigDiseno : Window
+    {
+        public ConfigDiseno()
+        {
+            InitializeComponent();
+            double MaxHeightScreen = SystemParameters.PrimaryScreenHeight;
+            double MaxWidthScreen = SystemParameters.PrimaryScreenWidth;
+            Top = (MaxHeightScreen/2) - (Height/2);
+            Left = MaxWidthScreen - Width;
+
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            Application.Current.MainWindow.WindowState = WindowState.Normal;
+        } 
+    }
+}
--- Precios_Turnos/MainWindow.xaml.cs
+++ Precios_Turnos/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,55 @@ namespace Precios_Turnos
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized)
+            {
+                WindowStyle = WindowStyle.None;
+                ResizeMode = ResizeMode.NoResize;
+                Visibility = Visibility.Collapsed;
+                Topmost = true;
+                Menu.Visibility= Visibility.Hidden;
+
+                //// re-show the window after changing style
+                Visibility = Visibility.Visible;
+                MaxHeight = SystemParameters.VirtualScreenHeight; 
+                MaxWidth = SystemParameters.VirtualScreenWidth;
+            }
+                
+            else
+            {
+                Topmost = false;
+                Menu.Visibility = Visibility.Visible;
+                ResizeMode = ResizeMode.CanResize;
+                WindowStyle = WindowStyle.ThreeDBorderWindow;
+            }
+               
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                WindowState = WindowState.Normal;
+            }
+
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ConfigurarDiseno_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            Topmost = false;
+            ConfigDiseno configDiseno = new ConfigDiseno();
+            configDiseno.ShowDialog();
+
         }
     }
 }
