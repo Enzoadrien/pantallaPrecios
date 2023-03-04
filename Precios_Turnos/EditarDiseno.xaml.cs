7a046945ae18f883312bc6ee6700a6d95b0be6f8
Arturo Murguia Espinoza <bofo_cona@hotmail.com>
2023-03-04 01:15:30 -0600

    Se realizan mejoras y correcciones

--- Precios_Turnos/EditarDiseno.xaml.cs
+++ Precios_Turnos/EditarDiseno.xaml.cs
@@ -20,15 +20,12 @@ namespace Precios_Turnos
     /// </summary>
     public partial class EditarDiseno : Window
     {
+        private MainWindow MainWindow;
 
-        public EditarDiseno()
+        public EditarDiseno(MainWindow mainWindow)
         {
             InitializeComponent();
-            double MaxHeightScreen = SystemParameters.PrimaryScreenHeight;
-            double MaxWidthScreen = SystemParameters.PrimaryScreenWidth;
-            Top = (MaxHeightScreen/2) - (Height/2);
-            Left = MaxWidthScreen - Width;
-
+            MainWindow = mainWindow;
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -39,8 +36,67 @@ namespace Precios_Turnos
         private void Salir_Click(object sender, RoutedEventArgs e)
         {
             Close();
-            Application.Current.MainWindow.WindowState = WindowState.Normal;
         }
 
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void cbxFuente_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var control = MainWindow.FindName(NombreControl.Text) as UIElement;
+            control.SetValue(FontFamilyProperty, new FontFamily(cbxFuente.SelectedItem.ToString()));
+        }
+
+        private void cbxTamano_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var control = MainWindow.FindName(NombreControl.Text) as UIElement;
+            control.SetValue(FontSizeProperty, Double.Parse(((ComboBoxItem)cbxTamano.SelectedItem).Tag.ToString()));
+        }
+
+        private void Contenido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Label control = (Label)MainWindow.FindName(NombreControl.Text);
+            control.Content = Contenido.Text;
+        }
+
+        private void chkNegrita_Checked(object sender, RoutedEventArgs e)
+        {
+            Label control = (Label)MainWindow.FindName(NombreControl.Text);
+            control.SetValue(FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void chkNegrita_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Label control = (Label)MainWindow.FindName(NombreControl.Text);
+            control.SetValue(FontWeightProperty, FontWeights.Normal);
+        }
+
+        private void chkCursiva_Checked(object sender, RoutedEventArgs e)
+        {
+            Label control = (Label)MainWindow.FindName(NombreControl.Text);
+            control.SetValue(FontStyleProperty, FontStyles.Italic);
+            
+        }
+
+        private void chkCursiva_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Label control = (Label)MainWindow.FindName(NombreControl.Text);
+            control.SetValue(FontStyleProperty, FontStyles.Normal);
+        }
+
+        private void btnPickColor_Click(object sender, RoutedEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Owner = this;
+            if ((bool)colorDialog.ShowDialog())
+            {
+                btnPickColor.Background = new SolidColorBrush(colorDialog.SelectedColor);
+            }
+        }
     }
 }
--- Precios_Turnos/MainWindow.xaml.cs
+++ Precios_Turnos/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +14,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Precios_Turnos
 {
@@ -24,10 +28,14 @@ namespace Precios_Turnos
     {
 
         private Point _positionInBlock;
+        private TranslateTransform? _currentTT;
+        private bool editar = false
+            ;
 
         public MainWindow()
         {
             InitializeComponent();
+            Coordenadas.Visibility = Visibility.Hidden;
         }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -37,7 +45,7 @@ namespace Precios_Turnos
                 WindowStyle = WindowStyle.None;
                 ResizeMode = ResizeMode.NoResize;
                 Visibility = Visibility.Collapsed;
-                Topmost = true;
+                //Topmost = true;
                 Menu.Visibility = Visibility.Hidden;
 
                 //// re-show the window after changing style
@@ -61,76 +69,170 @@ namespace Precios_Turnos
             if (e.Key == Key.Escape)
             {
                 WindowState = WindowState.Normal;
+                Coordenadas.Visibility = Visibility.Hidden;
+                ModoEdicion.Content = "Vista previa";
+                ModoEdicion.FontSize = 18;
+
+                if (editar)
+                    editar = false;
             }
 
         }
 
         private void Salir_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
         }
 
         private void EditarDiseno_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Maximized;
+            editar = true;
+            Coordenadas.Visibility = Visibility.Visible;
+            ModoEdicion.Content = "Modo edición";
+            ModoEdicion.FontSize = 24;
 
         }
 
-        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void Principal_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            try
-            {
-                var item = e.Source as UIElement;
-                if (item.GetType() == typeof(Label) || item.GetType() == typeof(Image))
+
+            if (editar)
+                try
                 {
-                    _positionInBlock = Mouse.GetPosition(item);
-                    item.CaptureMouse();
-
-                }
-            }
-            catch (Exception) { }
-
-        }
-
-        private void Window_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            try
-            {
-                var item = e.Source as UIElement;
-
-                if (item.GetType() == typeof(Label) || item.GetType() == typeof(Image))
-                {
-                    // release this control.
-                    item.ReleaseMouseCapture();
-
-                }
-            }
-            catch (Exception) { }
-
-        }
-
-        private void Window_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            try
-            {
-                var item = e.Source as UIElement;
-
-                if (item.GetType() == typeof(Label) || item.GetType() == typeof(Image))
-                {
-                    if (item.IsMouseCaptured)
+                    var item = e.Source as UIElement;
+                    if (SeModificaControl(item.GetValue(NameProperty).ToString()))
                     {
-                        // get the parent container
                         var container = VisualTreeHelper.GetParent(item) as UIElement;
-
-                        // get the position within the container
-                        var mousePosition = e.GetPosition(container);
-
-                        // move the usercontrol.
-                        item.RenderTransform = new TranslateTransform(mousePosition.X - _positionInBlock.X, mousePosition.Y - _positionInBlock.Y);
+                        _positionInBlock = e.GetPosition(container);
+                        item.CaptureMouse();
+                        _currentTT = item.RenderTransform as TranslateTransform;
                     }
                 }
+                catch (Exception) { }
+
+        }
+
+        private void Principal_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (editar)
+                try
+                {
+                    var item = e.Source as UIElement;
+
+                    if (SeModificaControl(item.GetValue(NameProperty).ToString()))
+                    {
+                        // release this control.
+                        item.ReleaseMouseCapture();
+                        _currentTT = item.RenderTransform as TranslateTransform;
+
+                    }
+                }
+                catch (Exception) { }
+
+        }
+
+        private void Principal_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (editar)
+                try
+                {
+                    var item = e.Source as UIElement;
+
+                    if (SeModificaControl(item.GetValue(NameProperty).ToString()))
+                    {
+                        if (item.IsMouseCaptured)
+                        {
+                            // get the parent container
+                            var container = VisualTreeHelper.GetParent(item) as UIElement;
+
+                            // get the position within the container
+                            var mousePosition = e.GetPosition(container);
+
+
+                            var offsetX = mousePosition.X - (_currentTT == null ? _positionInBlock.X : _positionInBlock.X - _currentTT.X);
+                            var offsetY = mousePosition.Y - (_currentTT == null ? _positionInBlock.Y : _positionInBlock.Y - _currentTT.Y);
+
+                            //var offsetX = mousePosition.X - (_currentTT == null ? _positionInBlock.X : _positionInBlock.X - _currentTT.X);
+                            //var offsetY = mousePosition.Y - (_currentTT == null ? _positionInBlock.Y : _positionInBlock.Y - _currentTT.Y);
+
+
+                            Coordenadas.Content = "Coordenadas: " + mousePosition.X + "X, " + mousePosition.Y + "Y";
+                            // move the usercontrol.
+                            item.RenderTransform = new TranslateTransform(offsetX, offsetY);
+                        }
+                    }
+                }
+                catch (Exception) { }
+        }
+
+
+        private bool SeModificaControl(string name)
+        {
+            bool seModifica;
+            switch (name)
+            {
+                case "Coordenadas":
+                case "ModoEdicion":
+                    seModifica = false;
+                    break;
+                default:
+                    seModifica = true;
+                    break;
             }
-            catch (Exception) { }
+            return seModifica;
+        }
+
+        private void Principal_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (editar)
+                try
+                {
+                    if (e.ClickCount == 2)
+                    {
+                        var item = e.Source as UIElement;
+
+                        if (SeModificaControl(item.GetValue(NameProperty).ToString()))
+                        {
+                            // get the parent container
+                            var container = VisualTreeHelper.GetParent(item) as UIElement;
+
+                            // get the position within the container
+                            var mousePosition = e.GetPosition(container);
+
+                            
+
+                            switch (item.GetType().ToString())
+                            {
+                                case "System.Windows.Controls.Label":
+                                    EditarDiseno editarDiseno = new EditarDiseno(this);
+                                    editarDiseno.WindowStartupLocation = WindowStartupLocation.Manual;
+                                    editarDiseno.Top = mousePosition.Y;
+                                    editarDiseno.Left = mousePosition.X;
+                                    editarDiseno.NombreControl.IsEnabled = false;
+                                    editarDiseno.NombreControl.Text = item.GetValue(NameProperty).ToString();
+                                    editarDiseno.cbxTipoControl.IsEnabled = false;
+                                    editarDiseno.cbxTipoControl.SelectedIndex = 0;
+                                    editarDiseno.Contenido.Text = ((Label)item).Content.ToString();
+                                    editarDiseno.cbxFuente.SelectedItem = item.GetValue(FontFamilyProperty);
+                                    editarDiseno.cbxTamano.SelectedValue = item.GetValue(FontSizeProperty);
+                                    editarDiseno.chkNegrita.IsChecked = item.GetValue(FontWeightProperty).ToString().CompareTo("Bold") == 0 ? true: false;
+                                    editarDiseno.chkCursiva.IsChecked = item.GetValue(FontStyleProperty).ToString().CompareTo("Italic") == 0 ? true : false;
+                                    editarDiseno.ShowDialog();
+                                    break;
+                                case "System.Windows.Controls.Image":
+                                    //editarDiseno.cbxTipoControl.SelectedIndex = 1;
+                                    break;
+                                default:
+
+                                    break;
+                            }
+
+                            
+                        }
+                    }
+                }
+                catch (Exception) { }
         }
     }
 }
