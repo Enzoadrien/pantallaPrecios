b3298f009a01eae68dda470eb37bdb49922bb80f
Arturo Murguia Espinoza <bofo_cona@hotmail.com>
2024-08-14 22:28:55 -0600

    Se realizan cambios para el servidor de activacion

---
+++ Precios_Turnos/ServerConfig.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
+
+namespace Precios_Turnos
+{
+    internal class ServerConfig
+    {
+        private readonly string serverKey = "Aplicaciones_3K_Mto_Prof";
+        private Seguridad vSeguridad = new Seguridad();
+        private string server = string.Empty;
+        private string database = string.Empty;
+        private string user = string.Empty;
+        private string password = string.Empty;
+
+        internal SqlConnection connection()
+        {
+            
+            SqlConnection connection;
+            try
+            {
+                var builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = vSeguridad.DecryptString(serverKey, recuperarValorServer("server")),
+                    InitialCatalog = vSeguridad.DecryptString(serverKey, recuperarValorServer("database")),
+                    UserID = vSeguridad.DecryptString(serverKey, recuperarValorServer("user")),
+                    Password = vSeguridad.DecryptString(serverKey, recuperarValorServer("password")),
+                };
+
+                connection = new SqlConnection(builder.ConnectionString);
+            } catch (Exception ex) {
+                connection = new SqlConnection();
+            }
+            return connection;
+        }
+        private String recuperarValorServer(string dato)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(@".\Recursos\server.3k");
+
+                XmlNode node = doc.DocumentElement.SelectSingleNode("/serverConfiguration");
+
+                foreach(XmlNode nodeChild in node.ChildNodes)
+                {
+                    if(nodeChild.Name.Equals(dato))
+                    {
+                        return nodeChild.InnerText;
+                    }
+                }
+            }
+            catch
+            {
+            }
+            return string.Empty;
+        }
+    }
+}
--- Precios_Turnos/ValidarLicencia.cs
+++ Precios_Turnos/ValidarLicencia.cs
@@ -5,43 +5,26 @@ using System.Net.Http;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.IO;
 using static Precios_Turnos.StateObject;
 using System.Windows;
 using MySqlX.XDevAPI;
 using System.Numerics;
+using Microsoft.Data.SqlClient;
 
 namespace Precios_Turnos
 {
     internal class ValidarLicencia
     {
         private Seguridad vSeguridad = new Seguridad();
-        private string server = "clERYmp7FWDroXEU1cTYAA==";
-        private string database = "FVlhqRNJp0HJEUXXY43bOJBU6hikYygRUXMc/e9dObY=";
-        private string user = "7Rl6MZ/paWHRlHou8wGlvw==";
-        private string password = "3Ro78Q+3C9v7aCF5BoRy4A==";
-        private string port = "BXg5UiO+Qm/gUKC+5Xltrg==";
-        private MySqlSslMode sslMode = MySqlSslMode.Required;
-
         internal string cargarLicenciaApp(string Correo, string Codigo)
         {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = vSeguridad.DecryptString(MainWindow.nombreApp, server),
-                Database = vSeguridad.DecryptString(MainWindow.nombreApp, database),
-                UserID = vSeguridad.DecryptString(MainWindow.nombreApp, user),
-                Password = vSeguridad.DecryptString(MainWindow.nombreApp, password),
-                Port = uint.Parse(vSeguridad.DecryptString(MainWindow.nombreApp, port)),
-                SslMode = sslMode,
-            };
-
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+            SqlConnection connection = new ServerConfig().connection();
             try
             {
                 connection.Open();
                 string llave = "";
-                using var command = new MySqlCommand("SELECT llave FROM licenciasApp WHERE correo='"+ Correo + "' AND codigo='"+ Codigo + "' AND nombreApp='"+MainWindow.nombreApp+"' AND activo=0;", connection);
+                using var command = new SqlCommand("SELECT llave FROM licenciasApp WHERE correo='"+ Correo + "' AND codigo='"+ Codigo + "' AND nombreApp='"+MainWindow.nombreApp+"' AND activo=0;", connection);
                 using var reader = command.ExecuteReader();
                 while (reader.Read())
                 {
@@ -59,7 +42,7 @@ namespace Precios_Turnos
 
                 return llave;
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
                 Mensajes dialogError = new Mensajes(Recursos.TipoMensaje.ERROR);
@@ -72,26 +55,17 @@ namespace Precios_Turnos
 
         internal bool activarLicenciaApp(string Correo, string Codigo)
         {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = vSeguridad.DecryptString(MainWindow.nombreApp, server),
-                Database = vSeguridad.DecryptString(MainWindow.nombreApp, database),
-                UserID = vSeguridad.DecryptString(MainWindow.nombreApp, user),
-                Password = vSeguridad.DecryptString(MainWindow.nombreApp, password),
-                Port = uint.Parse(vSeguridad.DecryptString(MainWindow.nombreApp, port)),
-                SslMode = sslMode,
-            };
 
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+            SqlConnection connection = new ServerConfig().connection();
             try
             {
                 connection.Open();
-                using var command = new MySqlCommand("UPDATE licenciasApp SET fechaActivacion ='"+ vSeguridad.GetNetworkTime().Date.ToString("yyyy-MM-dd") + "', activo=1 WHERE nombreApp='" + MainWindow.nombreApp + "'AND correo ='" + Correo + "'AND codigo='" + Codigo + "';", connection);
+                using var command = new SqlCommand("UPDATE licenciasApp SET fechaActivacion ='"+ vSeguridad.GetNetworkTime().Date.ToString("yyyy-MM-dd") + "', activo=1 WHERE nombreApp='" + MainWindow.nombreApp + "'AND correo ='" + Correo + "'AND codigo='" + Codigo + "';", connection);
                 using var reader = command.ExecuteReader();
                 connection.Close();
                 return true;
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
                 Mensajes dialogError = new Mensajes(Recursos.TipoMensaje.ERROR);
@@ -105,22 +79,12 @@ namespace Precios_Turnos
 
         internal string cargarLicenciaDiseno(string Correo, string Codigo)
         {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = vSeguridad.DecryptString(MainWindow.nombreApp, server),
-                Database = vSeguridad.DecryptString(MainWindow.nombreApp, database),
-                UserID = vSeguridad.DecryptString(MainWindow.nombreApp, user),
-                Password = vSeguridad.DecryptString(MainWindow.nombreApp, password),
-                Port = uint.Parse(vSeguridad.DecryptString(MainWindow.nombreApp, port)),
-                SslMode = sslMode,
-            };
-
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+            SqlConnection connection = new ServerConfig().connection();
             try
             {
                 connection.Open();
                 string llave = "";
-                using var command = new MySqlCommand("SELECT llave FROM licenciasDiseno WHERE correo='" + Correo + "' AND codigo='" + Codigo + "' AND nombreApp='" + MainWindow.nombreApp + "';", connection);
+                using var command = new SqlCommand("SELECT llave FROM licenciasDiseno WHERE correo='" + Correo + "' AND codigo='" + Codigo + "' AND nombreApp='" + MainWindow.nombreApp + "';", connection);
                 using var reader = command.ExecuteReader();
                 while (reader.Read())
                 {
@@ -139,7 +103,7 @@ namespace Precios_Turnos
 
                 return llave;
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
                 Mensajes dialogError = new Mensajes(Recursos.TipoMensaje.ERROR);
@@ -151,22 +115,13 @@ namespace Precios_Turnos
         }
         internal string cargarFechaActivacionLicenciaDiseno(string Correo, string Codigo, string Llave)
         {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = vSeguridad.DecryptString(MainWindow.nombreApp, server),
-                Database = vSeguridad.DecryptString(MainWindow.nombreApp, database),
-                UserID = vSeguridad.DecryptString(MainWindow.nombreApp, user),
-                Password = vSeguridad.DecryptString(MainWindow.nombreApp, password),
-                Port = uint.Parse(vSeguridad.DecryptString(MainWindow.nombreApp, port)),
-                SslMode = sslMode,
-            };
 
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+            SqlConnection connection = new ServerConfig().connection();
             try
             {
                 connection.Open();
                 string fechaActivacion = "";
-                using var command = new MySqlCommand("SELECT fechaActivacion, activo FROM licenciasDiseno WHERE correo='" + Correo + "' AND codigo='" + Codigo + "' AND llave='" + Llave + "' AND nombreApp='" + MainWindow.nombreApp + "';", connection);
+                using var command = new SqlCommand("SELECT fechaActivacion, activo FROM licenciasDiseno WHERE correo='" + Correo + "' AND codigo='" + Codigo + "' AND llave='" + Llave + "' AND nombreApp='" + MainWindow.nombreApp + "';", connection);
                 using var reader = command.ExecuteReader();
                 while (reader.Read())
                 {
@@ -188,7 +143,7 @@ namespace Precios_Turnos
 
                 return fechaActivacion;
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
                 Mensajes dialogError = new Mensajes(Recursos.TipoMensaje.ERROR);
@@ -201,26 +156,17 @@ namespace Precios_Turnos
 
         internal bool activarLicenciaDiseno(string Correo, string Codigo, string Llave)
         {
-            var builder = new MySqlConnectionStringBuilder
-            {
-                Server = vSeguridad.DecryptString(MainWindow.nombreApp, server),
-                Database = vSeguridad.DecryptString(MainWindow.nombreApp, database),
-                UserID = vSeguridad.DecryptString(MainWindow.nombreApp, user),
-                Password = vSeguridad.DecryptString(MainWindow.nombreApp, password),
-                Port = uint.Parse(vSeguridad.DecryptString(MainWindow.nombreApp, port)),
-                SslMode = sslMode,
-            };
 
-            MySqlConnection connection = new MySqlConnection(builder.ConnectionString);
+            SqlConnection connection = new ServerConfig().connection();
             try
             {
                 connection.Open();
-                using var command = new MySqlCommand("UPDATE licenciasDiseno SET fechaActivacion ='" + vSeguridad.GetNetworkTime().Date.ToString("yyyy-MM-dd") + "', activo=1 WHERE nombreApp='" + MainWindow.nombreApp + "'AND correo ='" + Correo + "'AND codigo='" + Codigo + "' AND llave='"+ Llave + "';", connection);
+                using var command = new SqlCommand("UPDATE licenciasDiseno SET fechaActivacion ='" + vSeguridad.GetNetworkTime().Date.ToString("yyyy-MM-dd") + "', activo=1 WHERE nombreApp='" + MainWindow.nombreApp + "'AND correo ='" + Correo + "'AND codigo='" + Codigo + "' AND llave='"+ Llave + "';", connection);
                 using var reader = command.ExecuteReader();
                 connection.Close();
                 return true;
             }
-            catch (MySqlException e)
+            catch (SqlException e)
             {
                 Console.WriteLine(e.Message);
                 Mensajes dialogError = new Mensajes(Recursos.TipoMensaje.ERROR);
