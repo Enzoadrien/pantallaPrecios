4397086322d4e7217c8d8a4e1ac86d761da4167b
Arturo Murguia Espinoza <bofo_cona@hotmail.com>
2024-09-02 16:23:07 -0600

    Se realiza cambio de nombre a app y se agrega icono

--- Precios_Turnos/ContenidoTabla.xaml.cs
+++ Precios_Turnos/ContenidoTabla.xaml.cs
@@ -1,24 +1,11 @@
-﻿using Microsoft.Win32;
-using Precios_Turnos.Properties;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Configuration;
 using System.Data;
-using System.Data.Common;
-using System.Data.Odbc;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Precios_Turnos
 {
